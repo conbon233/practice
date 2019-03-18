528db984bc6e12099d14eca82a5d101e804e1713
conbon233 <43069258+conbon233@users.noreply.github.com>
2019-03-18 08:56:46 -0500

    Add files via upload

---
+++ scripts/scripts-canvas.js
@@ -0,0 +1,8 @@
+var home = document.getElementById("home");
+var about = document.getElementById("about");
+var flavors = document.getElementById("flavors");
+var photoGallery = document.getElementById("photo-gallery");
+var orderOnline = document.getElementById("order-online");
+var contactUs = document.getElementById("contact-us");
+
+if ()
